d8a05455fcbb20ccc3d0e1eff1a1f16c34073372
aloftus3 <53193690+aloftus3@users.noreply.github.com>
2020-04-02 16:26:38 -0400

    button update

--- assignment_2/main.js
+++ assignment_2/main.js
@@ -5,13 +5,14 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
+
 for (let i = 1; i <= 5; i++) {
     const newImage = document.createElement('img');
     newImage.setAttribute('src','images/pic'+i+ '.jpg')
     thumbBar.appendChild(newImage);
     newImage.onclick = function(src_value){
-        displayedImage.src = src_value.target.src;    
-  }
+        displayedImage.src = src_value.target.src; 
+    }
 }
 /* 
 Image Sources:
@@ -22,6 +23,18 @@ pic4 - https://www.mypetwarehouse.com.au/my-pet-blog/expert-pet-advice/can-dogs-
 pic5 - https://en.wikipedia.org/wiki/File:Fernando_noronha.jpg
 
 */
-
-
-/* Wiring up the Darken/Lighten button */
+                                
+/* Wiring up the Darken/Lighten button */  
+/*MDN source used for base code*/
+btn.onclick = function() {
+    const btnClass = btn.getAttribute("class");
+    if (btnClass === "dark") {
+      btn.setAttribute("class", "light");
+      btn.textContent = "Lighten";
+      overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+    } else {
+      btn.setAttribute("class", "dark");
+      btn.textContent = "Darken";
+      overlay.style.backgroundColor = "rgba(0,0,0,0)";
+    }
+  }
