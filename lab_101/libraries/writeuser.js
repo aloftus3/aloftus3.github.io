33e4f7422f9a1c3334ca443b89ba259375d522ce
aloftus3 <53193690+aloftus3@users.noreply.github.com>
2020-04-24 10:30:18 -0400

    new update to INST377 repository

--- lab_101/libraries/writeuser.js
+++ lab_101/libraries/writeuser.js
@@ -6,15 +6,9 @@ async function writeUser(username, dbSettings) {
     const db = await open(dbSettings)
     await db.exec('CREATE TABLE IF NOT EXISTS user (name)');
     await db.exec(`INSERT INTO user VALUES ("${username}")`);
-    const result = await db.each('SELECT * FROM user', (err) => {
-        console.log('writeuser', err);
-    });
+    const result = await db.each('SELECT * FROM user');
     console.log('Expected result', result);
     return result;
   }
   
-export default writeUser;
-
-//   import writeUser from "./libraries/writeuser";
-
-// 
+  export default writeUser;
